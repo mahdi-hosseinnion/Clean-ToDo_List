5339b2acf7db5f643f97c41b4d7538c2a4015bd7
seyed mahdi <ssmmhh1382@gmail.com>
2021-07-27 10:26:32 +0430

    "017" SearchTasks use case

--- app/src/main/java/com/example/clean_todo_list/business/interactors/tasklist/SearchTasks.kt
+++ app/src/main/java/com/example/clean_todo_list/business/interactors/tasklist/SearchTasks.kt
@@ -1,4 +1,67 @@
 package com.example.clean_todo_list.business.interactors.tasklist
 
-class SearchTasks {
+import com.example.clean_todo_list.business.data.cache.CacheResponseHandler
+import com.example.clean_todo_list.business.data.cache.abstraction.TaskCacheDataSource
+import com.example.clean_todo_list.business.data.util.safeCacheCall
+import com.example.clean_todo_list.business.domain.model.Task
+import com.example.clean_todo_list.business.domain.state.*
+import com.example.clean_todo_list.framework.presentation.tasklist.state.TaskListViewState
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlin.reflect.jvm.internal.impl.types.TypeSubstitutionKt
+
+class SearchTasks(
+    private val taskCacheDataSource: TaskCacheDataSource
+) {
+
+    fun searchTasks(
+        query: String,
+        filterAndOrder: String,
+        page: Int,
+        stateEvent: StateEvent
+    ): Flow<DataState<TaskListViewState>?> = flow {
+        var updatedPage = page
+        if (page <= 0) {
+            updatedPage = 1
+        }
+        val cacheResult = safeCacheCall(IO) {
+            taskCacheDataSource.searchTask(
+                query, filterAndOrder, updatedPage
+            )
+        }
+
+        val cacheResponse = object : CacheResponseHandler<TaskListViewState, List<Task>>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override suspend fun handleSuccess(resultObj: List<Task>): DataState<TaskListViewState>? {
+                var message = SEARCH_NOTES_SUCCESS
+                var uiComponentType: UIComponentType = UIComponentType.None
+                if (resultObj.isEmpty()) {
+                    message = SEARCH_NOTES_NO_MATCHING_RESULTS
+                    uiComponentType = UIComponentType.Toast
+                }
+                return DataState.data(
+                    response = Response(
+                        message = message,
+                        uiComponentType = uiComponentType,
+                        messageType = MessageType.Success
+                    ),
+                    data = TaskListViewState(taskList = ArrayList(resultObj)),
+                    stateEvent = stateEvent
+                )
+            }
+        }.getResult()
+
+        emit(cacheResponse)
+
+    }
+
+    companion object {
+        val SEARCH_NOTES_SUCCESS = "Successfully retrieved list of notes."
+        val SEARCH_NOTES_NO_MATCHING_RESULTS = "There are no notes that match that query."
+        val SEARCH_NOTES_FAILED = "Failed to retrieve the list of notes."
+
+    }
 }
