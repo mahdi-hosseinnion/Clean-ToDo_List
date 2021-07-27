e059d90186fbadb8fddaf4d830b28b86699bb4b6
seyed mahdi <ssmmhh1382@gmail.com>
2021-07-27 12:32:35 +0430

    "018" Search tasks unit test

--- app/src/test/java/com/example/clean_todo_list/business/interactors/tasklist/SearchTasksTest.kt
+++ app/src/test/java/com/example/clean_todo_list/business/interactors/tasklist/SearchTasksTest.kt
@@ -1,8 +1,20 @@
 package com.example.clean_todo_list.business.interactors.tasklist
 
+import com.example.clean_todo_list.business.data.cache.CacheErrors.CACHE_ERROR_UNKNOWN
+import com.example.clean_todo_list.business.data.cache.FORCE_EXCEPTION
 import com.example.clean_todo_list.business.data.cache.abstraction.TaskCacheDataSource
 import com.example.clean_todo_list.business.data.network.abstraction.TaskNetworkDataSource
+import com.example.clean_todo_list.business.domain.model.Task
+import com.example.clean_todo_list.business.interactors.tasklist.SearchTasks.Companion.SEARCH_TASKS_NO_MATCHING_RESULTS
+import com.example.clean_todo_list.business.interactors.tasklist.SearchTasks.Companion.SEARCH_TASKS_SUCCESS
 import com.example.clean_todo_list.di.DependencyContainer
+import com.example.clean_todo_list.framework.datasource.database.ORDER_BY_ASC_DATE_UPDATED
+import com.example.clean_todo_list.framework.presentation.tasklist.state.TaskListStateEvent
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
 
 
 /**
@@ -45,4 +57,77 @@ class SearchTasksTest {
         )
     }
 
+    private val filterAndOrder = ORDER_BY_ASC_DATE_UPDATED
+
+    @Test
+    fun blankQuery_success_confirmAllTaskReceived() = runBlocking {
+        val query = ""
+        var result: ArrayList<Task>? = null
+
+        searchTasks.searchTasks(
+            query = query,
+            filterAndOrder = filterAndOrder,
+            page = 1,
+            stateEvent = TaskListStateEvent.SearchTasksEvent()
+        ).collect { dataState ->
+            assertEquals(
+                SEARCH_TASKS_SUCCESS,
+                dataState?.stateMessage?.response?.message
+            )
+            result = dataState?.data?.taskList
+        }
+        // confirm tasks were retrieved
+        assertTrue { result != null }
+        //check for data
+        val allTasksInCache = taskCacheDataSource.searchTask(query, filterAndOrder, 1)
+        assertEquals(allTasksInCache, result)
+    }
+
+    @Test
+    fun randomQuery_success_confirmNoMatchingResult() = runBlocking {
+        val query = "lasjfdk2947324jh2kl41u9471984kj4kl2j3kl;523524324jlkjlkasdjr324"
+        var result: ArrayList<Task>? = null
+
+        searchTasks.searchTasks(
+            query = query,
+            filterAndOrder = filterAndOrder,
+            page = 1,
+            stateEvent = TaskListStateEvent.SearchTasksEvent()
+        ).collect { dataState ->
+            assertEquals(
+                SEARCH_TASKS_NO_MATCHING_RESULTS,
+                dataState?.stateMessage?.response?.message
+            )
+            result = dataState?.data?.taskList
+        }
+        // confirm tasks were not retrieved
+        assertTrue { result?.isEmpty() ?: false }
+        //check if there is data in cache
+        val allTasksInCache = taskCacheDataSource.searchTask("", filterAndOrder, 1)
+        assertTrue { allTasksInCache.isNotEmpty() }
+    }
+
+    @Test
+    fun forceException_fail_confirmNoTaskReceived() = runBlocking {
+        val query = FORCE_EXCEPTION
+        var result: ArrayList<Task>? = null
+
+        searchTasks.searchTasks(
+            query = query,
+            filterAndOrder = filterAndOrder,
+            page = 1,
+            stateEvent = TaskListStateEvent.SearchTasksEvent()
+        ).collect { dataState ->
+            assertTrue {
+                dataState?.stateMessage?.response?.message?.contains(CACHE_ERROR_UNKNOWN) ?: false
+            }
+
+            result = dataState?.data?.taskList
+        }
+        // confirm tasks were not retrieved
+        assertTrue { result.isNullOrEmpty() }
+        //check if there is data in cache
+        val allTasksInCache = taskCacheDataSource.searchTask("", filterAndOrder, 1)
+        assertTrue { allTasksInCache.isNotEmpty() }
+    }
 }
