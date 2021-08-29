d7c0e2d99f946b9b76893b57c240992ab2bb83e6
seyed mahdi <ssmmhh1382@gmail.com>
2021-08-29 13:12:06 +0430

    auth use cases

---
+++ app/src/main/java/com/example/clean_todo_list/business/interactors/auth/forgotpassword/SendResetPasswordEmail.kt
@@ -0,0 +1,10 @@
+package com.example.clean_todo_list.business.interactors.auth.forgotpassword
+
+class SendResetPasswordEmail {
+
+    fun execute(
+        email: String,
+        password: String
+    ) {
+    }
+}
---
+++ app/src/main/java/com/example/clean_todo_list/business/interactors/auth/login/LoginUser.kt
@@ -0,0 +1,9 @@
+package com.example.clean_todo_list.business.interactors.auth.login
+
+class LoginUser {
+
+    fun execute(
+        email:String,
+        password:String
+    ){}
+}
