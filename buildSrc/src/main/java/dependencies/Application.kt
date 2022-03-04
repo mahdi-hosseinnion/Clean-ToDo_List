1cacea50e3463491ada90d034d253ac5502deb03
mahdi <ssmmhh1382@gmail.com>
2022-03-04 22:18:50 +0330

    update version name to 1.1.0

--- buildSrc/src/main/java/dependencies/Application.kt
+++ buildSrc/src/main/java/dependencies/Application.kt
@@ -2,6 +2,6 @@ package dependencies
 
 object Application {
     val id = "com.example.clean_todo_list"
-    val version_code = 1
-    val version_name = "1.0.0"
+    val version_code = 2
+    val version_name = "1.1.0"
 }
